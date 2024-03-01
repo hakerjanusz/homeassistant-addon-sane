304da3e8e09bf311641c3400d6e8768e9817b0b2
hakerjanusz <131874132+hakerjanusz@users.noreply.github.com>
2024-03-01 20:32:31 +0100

    Update run.sh

--- rootfs/run.sh
+++ rootfs/run.sh
@@ -2,9 +2,9 @@
 
 ulimit -n 1048576
 
-#until [ -e /var/run/avahi-daemon/socket ]; do
-#  sleep 1s
-#done
+until [ -e /var/run/avahi-daemon/socket ]; do
+  sleep 1s
+done
 
 bashio::log.info "Starting SANEd server as CMD from S6"
 
